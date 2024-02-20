847c1eb5312a6b47b63a4b12566ddc41ff76946f
sophia souza <sophia.souza@versatecnologia.com.br>
2024-02-20 10:48:18 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,14 +3,15 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
-    baseUrl: 'https://notes-serverless-app.com',
+  baseUrl: 'https://notes-serverless-app.com',
     env: {
       viewportWidthBreakpoint: 768,
     },
     requestTimeout: 50000,
     defaultCommandTimeout: 50000,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    //setupNodeEvents(on, config) {
+    // implement node event listeners here
+    //},
   },
+  projectId: 'hh9rfs',
 })
