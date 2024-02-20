cf986d96efcf05a7ca96685486a2bce3db5134d6
sophia souza <sophia.souza@versatecnologia.com.br>
2024-02-20 15:20:30 -0300

    Configure cypress grep

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,10 @@ module.exports = defineConfig({
     env: {
       viewportWidthBreakpoint: 768,
     },
+    setupNodeEvents(on, config) {
+      require('@cypress/grep/src/plugin')(config)
+      return config
+    },
     requestTimeout: 50000,
     defaultCommandTimeout: 50000,
   },
